555a88d113460eed02627f0cba90e68dc3db4aeb
Henyek <102858694+Henyek@users.noreply.github.com>
2024-09-12 22:39:23 +0200

    Added the first part of Bacpropagation algorithm

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from PIL import Image
 
-try:
-    import cupy as np
-except: 
-    import numpy as np
-
 from network import *
+
+import numpy as np
+from icecream import ic
+
 
 def show_image_from_array(array):
     array = np.clip(array, 0, 255).astype(np.uint8)
@@ -15,24 +14,34 @@ def show_image_from_array(array):
 def reLu(x):
     return np.maximum(x, 0)
 
+def reLu_derivative(x):
+    return x > 0
+
 def sigmoid(x):
     return 1/(1+np.exp(-x))
+
+def sigmoid_derivative(x):
+    return np.multiply(sigmoid(x), (1-sigmoid(x)))
 
 def tanh(x):
     return( (np.tanh(x) + 1) / 2 )
 
-GEOMETRY = [3072, 256, 256, 3072] # GEOMETRY is an array containing a 
+def tanh_derivative(x):
+    return((np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x)))
+
+GEOMETRY = [4, 2, 2, 4] # GEOMETRY is an array containing a 
                               # geometry of our neural network where
                               # first element is the number of inputs
                               # and last element is the number of outputs
 
 ACTIVATION_FUNCTIONS = [reLu, reLu, sigmoid]
+DERIVATIVES = [reLu_derivative, reLu_derivative, sigmoid_derivative]
 
 
 if __name__ == "__main__":
-    inputs = np.random.rand(32,32, 3)
+    inputs = np.array([1,-1,1,-1])
 
-    nn = Network(GEOMETRY, ACTIVATION_FUNCTIONS)
-
+    nn = Network(GEOMETRY, ACTIVATION_FUNCTIONS, DERIVATIVES)
     inputs = nn.fProp(inputs)
-    show_image_from_array(inputs.reshape(32, 32, 3) * 255)
+    nn.bProp(inputs, np.array([2, 3, 5, 2]))
+    # show_image_from_array(inputs.reshape(32, 32, 3) * 255)
--- network.py
+++ network.py
@@ -1,28 +1,43 @@
-try:
-    import cupy as np
-except: 
-    import numpy as np
-
-
+import numpy as np
+from icecream import ic
 
 class Network:
-    def __init__(self, GEOMETRY, activationFunctions):
+    def __init__(self, GEOMETRY, activationFunctions, derivatives):
         if len(GEOMETRY) - 1 == len(activationFunctions):
-            self.layers = []
+            self.weights = []
             for id, layer in enumerate(GEOMETRY[1:]):
                 W = np.array([np.random.uniform(low = -1, high = 1, size = (GEOMETRY[id], layer))])
-                self.layers.append(W)
+                self.weights.append(W)
             self.activationFunctions = activationFunctions
+            self.derivatives = derivatives
+            self.weightGradient = []
+            self.layerOutputs = []
+            self.nodeOutputs = []
         else:
             print("wronk")
             return False
 
 
+    def cost_function(self, values, target):
+        return np.subtract(target, values)**2
+
+
     def fProp(self, inputs):
         inputs = inputs.flatten()
-        for id, layer in enumerate(self.layers):
+        for id, layer in enumerate(self.weights):
             layer = np.array(layer).reshape(np.shape(layer[-1]))
             inputs = np.dot(inputs, layer)
             np.sum(inputs)
+            self.layerOutputs.append(inputs)
             inputs = self.activationFunctions[id](inputs)
+            self.nodeOutputs.append(inputs)
         return inputs
+    
+    def bProp(self, inputs, target):
+        baseGradient = np.multiply(2*(np.subtract(target, self.nodeOutputs[-1])),self.derivatives[-1](self.layerOutputs[-1]))
+        layerGradient = []
+        for x in self.nodeOutputs[-2]:
+            layerGradient.append(baseGradient * x)
+        self.weightGradient.insert(0, layerGradient)# base gradient is the first part of the equation before multiplying by the values of the layer before
+
+        #choose weight and iterate over all nodes in the layer before
